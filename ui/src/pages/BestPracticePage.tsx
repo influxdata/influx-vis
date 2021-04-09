3903624e5559b512cba286066ca7b48279068f0e
ivankudibal <8028110+ivankudibal@users.noreply.github.com>
2021-04-09 17:30:57 +0200

    fix: documentationi and review.. Closes #1

--- ui/src/pages/BestPracticePage.tsx
+++ ui/src/pages/BestPracticePage.tsx
@@ -2,30 +2,6 @@ import { Card, Col, PageHeader, Row } from "antd";
 import React from "react";
 import { SimplificationDemo } from "../views/SimplificationDemo";
 
-const bestPracticesText = `\
-When dealing with bigger datasets, there are problems that need to be solved to make good user interface. 
-In this situation we encounter two problems - speed and memory consuption.
-Both this problems can affect slower or even high-end devices, so we need to solve them to have more inclusive sotware because 
-lagging of ui can be one of barriers to entre influxdb world and can scare off possible customers.
-
-One way to (at least partialy) solve this problem is simplification algorithm that prone points from line, 
-that provides detail almost not visible for human eye therefore they are useless for visualisations.
-Simplifying of line results in smaller amount of points, so drawing line won't cause browser to lag 
-and original data can be removed from client browser which can help performance too.
-This algorithm can be implemented on both server and client with following +:
- - server
-  + less internet connection
-  + less data on user browser
- - client
-  + same selects as before
-    + existing software can just add this alg and visualisations without need to change queries
-    + don't add any more load on server
-  + all data present on browser for other work 
-    + show tables
-    + transform data on client to do some more different visualisations and analysis
-`;
-
-
 type TBestPracticePageProps = {
 
 };
@@ -39,8 +15,67 @@ export const BestPracticePage: React.FC<TBestPracticePageProps> = (props) => {
     >
       <Row gutter={[12, 12]}>
         <Col xs={24}>
-          <Card>
-            <pre>{bestPracticesText}</pre>
+          <Card title="Problem Statement">
+            <div>
+
+              <p>When dealing with, bigger time series datasets, some problems need to be
+              solved to make a good user interface.</p>
+
+            </div>
+
+            <div>
+
+              <p>In this situation, we encounter two problems - <b>speed</b> and <b>memory</b> consumption.</p>
+
+            </div>
+
+            <div>
+
+              <p>Both these problems slow down even high-end devices, so we need
+              to solve them because the lagging of the UI layer can be one of
+              the barriers to visualize InfluxDB queries in your application and
+              can scare off its users.</p>
+
+            </div>
+            <div>
+
+              <p>One way to, at least partially, solve this problem is to
+              simplify the rendering of the line by removing those points that
+              provide a level of detail almost not visible for the human eye,
+              therefore they are useless for visualizations.</p>
+
+            </div>
+            <div>
+
+              <p>Line simplification results in a smaller amount of points, so
+              drawing lines won't cause the browser to lag and original data can
+              be removed from the client browser. Both significantly improve the
+              performance of a browser.</p>
+
+            </div>
+            <div>
+
+              <p>This algorithm can be implemented on both server and client
+              with the following beefits:</p>
+
+            </div>
+            <div>
+              <ul>
+                <li>server</li>
+                  <ul>
+                    <li>low-end internet connection</li>
+                    <li>less data on user browser</li>
+                  </ul>
+                  <li>client</li>
+                  <ul>
+                    <li>all server side benefits mentioned above</li>
+                    <li>the algorithm can be added with no need to change queries</li>
+                    <li>don't add any more load on server</li>
+                    <li>all data remain on browser for other work, such as tables, or other analytic transformations</li>
+                  </ul>
+              </ul>
+            </div>
+
           </Card>
         </Col>
         <Col xs={24}>
--- ui/src/pages/HomePage.tsx
+++ ui/src/pages/HomePage.tsx
@@ -4,16 +4,83 @@ import React from "react";
 export const HomePage: React.FC = () => {
 
   return <>
-    <Card title="Example project for visualisation with influxDB backend">
-      This project is example how to use influxdata as source for different graph/visualisation libraries. 
-      <div />
-      Function "useInfluxSource" simulates influxdb influx source through csv influx together with user inputed select of which columns is selected to group values. UI has also buildin generator for random csv lines.
-      <div />
-      Only not-pivoted data (values stored in "_value" column) are supported for now.
-      <div />
-      <strong>normalizedDataFromTable</strong> show proposed function for easier more "JavaScript-ish" working with incoming influx data. There should be only simple transformation needed after data normalization function.
-      <div />
-      Simplification algorithm presented on Best practice tab is <strong>not</strong> used for libraries (original data is used)
+    <Card title="Visualize InfluxDB query results by using the 3rd party React components">
+
+      <div>
+
+        <p>This project is a <strong>playground for React front
+        developers</strong> that look for ways to visualize
+        <strong>InfluxDB</strong> query results by using the 3rd party
+        <strong>React</strong> components. For the <strong>dashboard</strong> use case, the
+        Influxdata Giraffe project is not always an option. Alternatively, some
+        of the most popular graph/visualization React-based data visualization
+        libraries are easy to use, too. Here we present <strong>Nivo</strong>
+        and <strong>Recharts</strong>. This is what you find at the Nivo and
+        Recharts tabs.</p>
+
+      </div>
+
+      <h3>How to Use this Application</h3>
+      <div>
+        <p>To start, follow the procedure:</p>
+        <ol>
+          <li>Open one of the two tabs located in the header - <strong>Recharts</strong> or <strong>Nivo</strong>.</li>
+          <li>Select sample data from the dropdown labeled <strong>Preset'</strong>.</li>
+          <li>The line chart shows up. Its properties can be modified by UI controls in the middle.</li>
+          <ul>
+            <li>Copy paste your CSV, that you get from Influx DB.</li>
+            <li>Generate random points, number of lines, density etc. Find the performance limits of <strong>SVG</strong> and <strong>canvas</strong> on your computer.</li>
+            <li>Group the data in the chart by columns</li>
+          </ul>
+          <li>The source code used to display the chart is shown on the right side.</li>
+          <li>The same data is rendered in the table in the last row.</li>
+        </ol>
+      </div>
+
+      <h3>Performance and Scalability</h3>
+      <div>
+
+        <p>To plot lines, SVG finds the performance limits first. This applies
+        to 50K points. In Nivo, there is the option to use a better-performing
+        canvas. However, for dashboards, replacing <strong>SVG</strong> with <strong>canvas</strong> to render
+        data faster is not the complete answer
+        to acceptable performance. The canvas object starts hitting its limit
+        with 100-300K points.</p>
+
+      </div>
+
+      <div>
+        <p>The last tab called <strong>Best practice</strong> shows how to <strong>build high-performing and scalable charts</strong>.</p>
+      </div>
+
+      <h3>How is the application designed</h3>
+
+      <div>
+
+        <p>There is a function called <code>useInfluxSource</code>. It
+        represents the InfluxDB source. The implementation provides data in the
+        format of annotated CSVs that are served by the database. The
+        application has also a built-in data generator of random CSV lines.</p>
+
+      </div>
+
+      <div>
+
+        <p>The demo renders a chart that plots the data stored in the column
+        titled <strong>_value</strong>. There is the UI control demonstrating
+        how the data fields can be aggregated.</p>
+
+      </div>
+
+      <div>
+
+        <p>To plot the chart, the data must be transformed into data structures
+        that are natively consumable by chart objects. In the source code, look
+        up the function called <code>normalizedDataFromTable</code>. The
+        function makes it easy to work with incoming InfluxDB data.</p>
+
+      </div>
+
     </Card>
   </>;
 };
